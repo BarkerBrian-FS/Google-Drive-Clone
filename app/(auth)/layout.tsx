9a27037b5c86ac4235c1058c5af62822a9744e13
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-13 10:59:31 -0500

    added logo and files image to auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Image from "next/image";
-import logo from "@/app/StoreItCircle.ico";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
       <section className="bg-brand p-10">
         <div>
-          <Image src={logo} alt="logo" width={40} height={40} />
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={224}
+            height={84}
+          />
           <div className="space-y-5 text-white">
             <h1 className="h1">Manage your files the best way</h1>
             <p className="body-1">
@@ -15,6 +19,13 @@ const layout = ({ children }: { children: React.ReactNode }) => {
             </p>
           </div>
         </div>
+        <Image
+          src="/assets/images/files.png"
+          alt="files"
+          width={342}
+          height={342}
+          className="transition-all hover:rotate-2 hover:scale-105"
+        />
       </section>
       {children}
     </div>
