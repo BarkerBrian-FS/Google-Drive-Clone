27da057ea660fa6f135a9611eb34742273faa242
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-14 12:16:54 -0500

    moved pages from app to root

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,10 +3,9 @@ import React from "react";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex h-screen">
-      <Sidebar />
+      Sidebar
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
-        <Header />
+        Mobile Navigation Header
         <div className="main-content">{children}</div>
       </section>
     </main>
