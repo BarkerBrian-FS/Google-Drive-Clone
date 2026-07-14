95812b478cbb5993a9426d3535044b6d2d2b5d69
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-14 13:32:55 -0500

    added access to current user using get user function

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,8 +2,14 @@ import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import MobileNavigation from "@/components/MobileNavigation";
 import React from "react";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
+
   return (
     <main className="flex h-screen">
       <Sidebar />
