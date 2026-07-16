346059208b8139bd2e26541de56100e15a965967
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-16 12:00:29 -0500

    loader not working need to debug

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -104,7 +104,7 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
                       width={80}
                       height={26}
                       alt="Loader"
-                      // unoptimized
+                      unoptimized
                     />
                   </div>
                 </div>
