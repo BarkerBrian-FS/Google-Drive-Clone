43bc35f417c310a1bfa743dab909320198eed884
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-16 12:41:59 -0500

    created dynamic route to display all files

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,26 @@
+import Sort from "@/components/Sort";
+import { SearchParamProps } from "@/types";
+import React from "react";
+
+const page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+
+  return (
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0mb</span>
+          </p>
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort By:</p>
+            <Sort />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
