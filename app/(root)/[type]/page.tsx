f4f6bec90bf5aca1a9f84343dc2b91a29afb144a
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-17 13:28:07 -0500

    fixed bug with get files by type

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,16 +1,17 @@
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
-import { SearchParamProps } from "@/types";
+import { getFileTypesParams } from "@/lib/utils";
+import { FileType, SearchParamProps } from "@/types";
 import { Models } from "node-appwrite";
 import React from "react";
 
 const page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
 
-  const types = getFileTypeParams(type) as FileType[];
+  const types = getFileTypesParams(type) as FileType[];
 
-  const files = await getFiles({ types}:{types});
+  const files = await getFiles({ types });
 
   return (
     <div className="page-container">
