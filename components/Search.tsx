9df01e1e778efb9336ccdd01ede5c80d6268cbf6
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-18 11:01:19 -0500

    working on search bar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query");
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({ types: [], searchText: query });
+
+      setResults(files.documents);
+      setOpen(true);
+    };
+    fetchFiles();
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="search"
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li key={file.$id} className="flex items-center">
+                  <div className="flex cursor-pointer gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                  </div>
+                  <p className="subtitle-2 line-clamp-1 text-light-100">
+                    {file.name}
+                  </p>
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No Files Found</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
